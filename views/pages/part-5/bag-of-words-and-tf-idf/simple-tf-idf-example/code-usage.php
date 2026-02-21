6ea3fd69dd755693bccd3e0abdc5d4532dceca19
Samuel Akopyan <samuel.akopyan@thrivedx.com>
2026-02-21 22:26:52 +0200

    Added Bag of Words и TF–IDF

---
+++ views/pages/part-5/bag-of-words-and-tf-idf/simple-tf-idf-example/code-usage.php
@@ -0,0 +1,98 @@
+<?php
+
+// Source documents
+$documents = [
+    'кот ест рыбу',
+    'кот любит рыбу',
+    'собака ест мясо из консервов',
+];
+
+// --------------------
+// Tokenization
+// --------------------
+function tokenize(string $text): array {
+    return explode(' ', $text);
+}
+
+$tokenized = array_map('tokenize', $documents);
+
+// --------------------
+// Build vocabulary
+// --------------------
+$vocab = [];
+foreach ($tokenized as $doc) {
+    foreach ($doc as $word) {
+        $vocab[$word] = true;
+    }
+}
+$vocab = array_keys($vocab);
+
+// --------------------
+// Term Frequency (TF)
+// --------------------
+function termFrequency(array $doc): array {
+    $tf = [];
+    $length = count($doc);
+
+    foreach ($doc as $word) {
+        $tf[$word] = ($tf[$word] ?? 0) + 1;
+    }
+
+    foreach ($tf as $word => $count) {
+        $tf[$word] = $count / $length;
+    }
+
+    return $tf;
+}
+
+// --------------------
+// Document Frequency + IDF
+// --------------------
+function documentFrequency(array $tokenized): array {
+    $df = [];
+
+    foreach ($tokenized as $doc) {
+        foreach (array_unique($doc) as $word) {
+            $df[$word] = ($df[$word] ?? 0) + 1;
+        }
+    }
+
+    return $df;
+}
+
+$df = documentFrequency($tokenized);
+$N  = count($tokenized);
+
+$idf = [];
+foreach ($df as $word => $count) {
+    $idf[$word] = log($N / $count);
+}
+
+// --------------------
+// TF–IDF
+// --------------------
+function tfidf(array $tf, array $idf): array {
+    $vector = [];
+
+    foreach ($tf as $word => $value) {
+        $vector[$word] = $value * ($idf[$word] ?? 0);
+    }
+
+    return $vector;
+}
+
+$tfidfVectors = [];
+foreach ($tokenized as $doc) {
+    $tf = termFrequency($doc);
+    $tfidfVectors[] = tfidf($tf, $idf);
+}
+
+echo 'Vocabulary: ' . implode(', ', $vocab) . PHP_EOL . PHP_EOL;
+
+foreach ($tfidfVectors as $i => $vector) {
+    echo "Документ " . ($i + 1) . ":" . PHP_EOL;
+    foreach ($vector as $word => $value) {
+        echo "  $word => " . round($value, 3) . PHP_EOL;
+    }
+    echo PHP_EOL;
+}
---
+++ views/pages/part-5/bag-of-words-and-tf-idf/simple-tf-idf-example/index.php
@@ -0,0 +1,16 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2"><?= __t('bag_of_words_and_tf_idf.simple_tfidf_example'); ?></h1>
+</div>
+
+<?= create_run_code_button(__t('bag_of_words_and_tf_idf.simple_tfidf_example'), 'part-5/bag-of-words-and-tf-idf/simple-tf-idf-example/code-run'); ?>
+
+<div>
+    <p>
+        <?= __t('bag_of_words_and_tf_idf.simple_tfidf_example_intro'); ?>
+    </p>
+</div>
+
+<div>
+    <?= create_example_of_use_links(__DIR__ . '/code.php', opened: true, copyButtonId: 1, expandButtonId: 1); ?>
+</div>
+<br><br>
